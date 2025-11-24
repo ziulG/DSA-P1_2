252cf89068eb617b1c53ff73a831c8d0bdd8f381
ziulG <lgcutrim2004@gmail.com>
2025-11-24 12:18:52 -0300

    Fix typo in output message in Main.java for improved consistency in presentation.

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println("╔═══════════════════════════════════════════════════════════╗");
-            System.out.println("║   SISTEMA DE ORDENAÇÃO ESTÁVEL DE COMMITS - ED II        ║");
+            System.out.println("║   SISTEMA DE ORDENAÇÃO ESTÁVEL DE COMMITS - ED II         ║");
             System.out.println("╚═══════════════════════════════════════════════════════════╝\n");
             
             String[] arquivos = {
